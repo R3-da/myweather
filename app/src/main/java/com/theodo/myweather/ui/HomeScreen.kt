f2e27e849dace07ce36bfb094885efd992a5f630
r3-da <haddan.reda.net@gmail.com>
2025-01-13 08:46:05 +0100

    Refactor HomeScreen to implement sticky headers for date display; enhance layout with background color for improved visibility

--- app/src/main/java/com/theodo/myweather/ui/HomeScreen.kt
+++ app/src/main/java/com/theodo/myweather/ui/HomeScreen.kt
@@ -4,8 +4,11 @@ package com.theodo.myweather.ui
 
 import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -16,6 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -46,8 +50,7 @@ fun HomeScreen(
     ) {
         LazyColumn(
             modifier = Modifier
-            .fillMaxSize()
-            .padding(8.dp)
+                .fillMaxSize()
         ) {
             when (weatherResp.value) {
                 is StateResource.Loading -> {
@@ -62,10 +65,16 @@ fun HomeScreen(
                     if (groupedData.isNotEmpty()) {
                         // Iterate over each date and display the intervals grouped by date
                         groupedData.forEach { (date, intervals) ->
-                            item {
-                                HeadingTextComposable(
-                                    textValue = date
-                                )
+                            stickyHeader {
+                                Box(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .background(Color.White) // Set the background color to white
+                                ) {
+                                    HeadingTextComposable(
+                                        textValue = date
+                                    )
+                                }
                             }
 
                             items(intervals) { interval ->
